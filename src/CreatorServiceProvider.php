3288b16c62b4930eb796aba9f938ab82c69fb648
Pp Morales <ppjmorales@gmail.com>
2022-01-04 20:32:27 -0600

    second commit

--- src/CreatorServiceProvider.php
+++ src/CreatorServiceProvider.php
@@ -32,7 +32,7 @@ class CreatorServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $this->loadRoutesFrom(__DIR__.'routes/web.php');
+        $this->loadRoutesFrom(__DIR__.'/routes/web.php');
         $this->loadMigrationsFrom(__DIR__.'/database/migrations');
 
         $this->publishes([
