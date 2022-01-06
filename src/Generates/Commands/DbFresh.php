1bcb9d13fb4292255a7233ee6946e1ad92c24116
Pp Morales <ppjmorales@gmail.com>
2022-01-05 18:59:42 -0600

    second commit

---
+++ src/Generates/Commands/DbFresh.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Pp\Creator\Generates\Commands;
+
+
+use Illuminate\Console\Command;
+use Pp\Creator\Models\Menu;
+
+class DbFresh extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'db:fresh --seed';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $menus = Menu::all();
+        $this->call('migrate:fresh', [
+            '--seed' => $this->option('seed')
+        ]);
+        Menu::insert($menus->toArray());
+        $this->info('El menu fue reestablecido');
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        return 0;
+    }
+}
